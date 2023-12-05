f18da777c21b1454204047e72567838936d4d685
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-05 12:11:39 +0100

    Updated PhoneBook

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 11:08:51 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/04 23:20:25 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/05 12:02:40 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 PhoneBook::PhoneBook()
 {
-	this->_oldestIndex = 0;
+	this->oldestIndex = 0;
 
 	return;
 }
@@ -26,13 +26,13 @@ PhoneBook::~PhoneBook()
 
 Contact *PhoneBook::wipeContact(int index)
 {
-	this->_contacts[index].setFirstName("");
-	this->_contacts[index].setLastName("");
-	this->_contacts[index].setNickname("");
-	this->_contacts[index].setPhoneNumber("");
-	this->_contacts[index].setDarkestSecret("");
+	this->contacts[index].setFirstName("");
+	this->contacts[index].setLastName("");
+	this->contacts[index].setNickname("");
+	this->contacts[index].setPhoneNumber("");
+	this->contacts[index].setDarkestSecret("");
 
-	return &this->_contacts[index];
+	return &this->contacts[index];
 }
 
 void PhoneBook::addContact()
@@ -40,7 +40,7 @@ void PhoneBook::addContact()
 	Contact *contact;
 	std::string input;
 
-	contact = this->wipeContact(this->_oldestIndex);
+	contact = this->wipeContact(this->oldestIndex);
 	std::cout << "first name: ";
 	std::getline(std::cin, input);
 	contact->setFirstName(input);
@@ -58,7 +58,7 @@ void PhoneBook::addContact()
 	contact->setDarkestSecret(input);
 
 	if (contact->isValid())
-		this->_oldestIndex = (this->_oldestIndex + 1) % 8;
+		this->oldestIndex = (this->oldestIndex + 1) % 8;
 	else
 	{
 		std::cerr << "A contact cannot have empty fields !" << std::endl;
@@ -79,11 +79,11 @@ void PhoneBook::previewContacts() const
 	{
 		std::cout << std::setfill(' ') << "|" << std::setw(10) << i;
 		std::cout << "|" << std::setw(10);
-		std::cout << this->_truncate(_contacts[i].getFirstName());
+		std::cout << this->truncate(contacts[i].getFirstName());
 		std::cout << "|" << std::setw(10);
-		std::cout << this->_truncate(_contacts[i].getLastName());
+		std::cout << this->truncate(contacts[i].getLastName());
 		std::cout << "|" << std::setw(10);
-		std::cout << this->_truncate(_contacts[i].getNickname());
+		std::cout << this->truncate(contacts[i].getNickname());
 		std::cout << "|" << std::endl;
 		std::cout << std::setfill('-') << std::setw(45) << "" << std::endl;
 	}
@@ -96,7 +96,7 @@ void PhoneBook::showContactDetails(int index) const
 		std::cerr << "Error: invalid index" << std::endl;
 		return;
 	}
-	else if (!this->_contacts[index].isValid())
+	else if (!this->contacts[index].isValid())
 	{
 		std::cerr << "Error: no contact at index `";
 		std::cerr << index << "'" << std::endl;
@@ -104,18 +104,18 @@ void PhoneBook::showContactDetails(int index) const
 	}
 
 	std::cout << "first name: \"";
-	std::cout << this->_contacts[index].getFirstName() << "\"" << std::endl;
+	std::cout << this->contacts[index].getFirstName() << "\"" << std::endl;
 	std::cout << "last name: \"";
-	std::cout << this->_contacts[index].getLastName() << "\"" << std::endl;
+	std::cout << this->contacts[index].getLastName() << "\"" << std::endl;
 	std::cout << "nickname: \"";
-	std::cout << this->_contacts[index].getNickname() << "\"" << std::endl;
+	std::cout << this->contacts[index].getNickname() << "\"" << std::endl;
 	std::cout << "phone number: \"";
-	std::cout << this->_contacts[index].getPhoneNumber() << "\"" << std::endl;
+	std::cout << this->contacts[index].getPhoneNumber() << "\"" << std::endl;
 	std::cout << "darkest secret: \"";
-	std::cout << this->_contacts[index].getDarkestSecret() << "\"" << std::endl;
+	std::cout << this->contacts[index].getDarkestSecret() << "\"" << std::endl;
 }
 
-std::string PhoneBook::_truncate(std::string const str) const
+std::string PhoneBook::truncate(std::string const str) const
 {
 	if (str.length() <= 10)
 		return str;
