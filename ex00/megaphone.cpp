3bc7456f1e7956e84c8cf1be24418c7723b1ef40
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-06 16:39:07 +0100

    Updated megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,27 +6,31 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 20:49:45 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/05 21:22:45 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/06 16:29:32 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <locale>
+#include <string>
 
 int main(int argc, char *argv[])
 {
-	if (argc > 1)
+	std::locale loc;
+	std::string str;
+
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
 	{
 		for (int i = 1; i < argc; ++i)
 		{
-			while (*argv[i])
-			{
-				std::cout << static_cast<char>(toupper(*argv[i]));
-				++argv[i];
-			}
+			str = argv[i];
+
+			for (std::size_t n = 0; n < str.length(); ++n)
+				std::cout << std::toupper(str.at(n), loc);
 		}
 	}
-	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
 	std::cout << std::endl;
 
