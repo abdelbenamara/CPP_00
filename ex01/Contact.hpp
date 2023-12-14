8e7ccd5a1e43f77287100195487226234f7b7c4f
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-14 13:02:28 +0100

    Updated Contact

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,32 +6,32 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 10:25:09 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/05 20:39:28 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/14 13:00:37 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CONTACT_HPP
-#define CONTACT_HPP
+#ifndef __CONTACT_HPP__
+#define __CONTACT_HPP__
 
 #include <string>
 
 class Contact
 {
 public:
-	Contact();
-	~Contact();
+	Contact(void);
+	~Contact(void);
 
-	std::string getFirstName() const;
+	std::string getFirstName(void) const;
 	void setFirstName(std::string firstName);
-	std::string getLastName() const;
+	std::string getLastName(void) const;
 	void setLastName(std::string lastName);
-	std::string getNickname() const;
+	std::string getNickname(void) const;
 	void setNickname(std::string nickname);
-	std::string getPhoneNumber() const;
+	std::string getPhoneNumber(void) const;
 	void setPhoneNumber(std::string phoneNumber);
-	std::string getDarkestSecret() const;
+	std::string getDarkestSecret(void) const;
 	void setDarkestSecret(std::string darkestSecret);
-	bool isValid() const;
+	bool isValid(void) const;
 
 private:
 	std::string firstName;
