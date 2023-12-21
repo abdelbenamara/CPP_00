a7dd2ca7747ee79e0d34ecf7c19d52a348c88684
Abdel Benamara <abdelb.contact@gmail.com>
2023-12-21 13:50:14 +0100

    Updated Contact

--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 10:25:09 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/21 12:53:51 by abenamar         ###   ########.fr       */
+/*   Updated: 2023/12/21 13:26:53 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,15 +22,15 @@ public:
 	~Contact(void);
 
 	std::string const &getFirstName(void) const;
-	void setFirstName(std::string firstName);
+	void setFirstName(std::string const &firstName);
 	std::string const &getLastName(void) const;
-	void setLastName(std::string lastName);
+	void setLastName(std::string const &lastName);
 	std::string const &getNickname(void) const;
-	void setNickname(std::string nickname);
+	void setNickname(std::string const &nickname);
 	std::string const &getPhoneNumber(void) const;
-	void setPhoneNumber(std::string phoneNumber);
+	void setPhoneNumber(std::string const &phoneNumber);
 	std::string const &getDarkestSecret(void) const;
-	void setDarkestSecret(std::string darkestSecret);
+	void setDarkestSecret(std::string const &darkestSecret);
 	bool isValid(void) const;
 
 private:
